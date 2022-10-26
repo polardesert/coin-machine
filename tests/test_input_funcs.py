4a3ebe8a96521965d74dd7d88f0157a7db7a9ab2
selphaware-ahmadus <uahmad3013@outlook.com>
2022-10-27 00:24:19 +0100

    test input funcs for £ and $

--- tests/test_input_funcs.py
+++ tests/test_input_funcs.py
@@ -1,2 +1,30 @@
+#!/usr/bin/env python
+"""
+
+"""
+
 import unittest
 from parameterized import parameterized
+from src.input_funcs import cnvt_str_int
+
+
+class TestInputFuncs(unittest.TestCase):
+    @parameterized.expand([
+        ["£-", 0], ["£0-", 0], ["£-0", 0], ["£1234567-98", 123456798],
+        ["£1000-23", 100023], ["£4-5", 405], ["£4-05", 405], ["£4-50", 450],
+        ["£0-43", 43], ["£0-3", 3]
+    ])
+    def test_pound_input_funcs(self, in_str_val: str, out_exp_val: int):
+        int_val, ccy_symbol = cnvt_str_int(in_str_val)
+        self.assertEqual(int_val, out_exp_val)
+        self.assertEqual(ccy_symbol, "£")
+
+    @parameterized.expand([
+        ["$-", 0], ["$0-", 0], ["$-0", 0], ["$1234567-98", 123456798],
+        ["$1000-23", 100023], ["$4-5", 405], ["$4-05", 405], ["$4-50", 450],
+        ["$0-43", 43], ["$0-3", 3]
+    ])
+    def test_dollar_input_funcs(self, in_str_val: str, out_exp_val: int):
+        int_val, ccy_symbol = cnvt_str_int(in_str_val)
+        self.assertEqual(int_val, out_exp_val)
+        self.assertEqual(ccy_symbol, "$")
