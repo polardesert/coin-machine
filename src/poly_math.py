a8b95fcf34a7b1cf63f32b521b4a0e519a007e07
selphaware-ahmadus <uahmad3013@outlook.com>
2022-10-28 00:03:02 +0100

    refactor term variable in poly_math

--- src/poly_math.py
+++ src/poly_math.py
@@ -32,8 +32,8 @@ def poly_divide(func: Generator, poly: List[int]):
     numer = [next(func) for _ in range(len(poly) - 1)]
     numer.append(0)  # this is the location "bringing down" the next term of f goes
 
-    for fj in func:
-        numer[-1] = fj
+    for term in func:
+        numer[-1] = term
         coeff = numer[0] // poly[0]
 
         # assert all terms are integers
